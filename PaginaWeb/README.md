e720d04eb43782c588dee560a73e11cc970d16ae
Shambles <102494732+ShamblesLaw@users.noreply.github.com>
2022-10-27 12:33:58 -0300

    Add files via upload

---
+++ PaginaWeb/README.md
@@ -0,0 +1,4 @@
+# PaginaWeb
+Primera versión de la Web de Técnica Bella Unión
+
+https://youtu.be/SSPdr2jHtIs
---
+++ PaginaWeb/js/script.js
@@ -0,0 +1,78 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let imagenes = [
+        {img: '../img/galeria/1.jpeg'},
+        {img: '../img/galeria/2.jpg'},
+        {img: '../img/galeria/3.jpg'},
+        {img: '../img/galeria/4.jpg'},
+        {img: '../img/galeria/5.jpg'},
+        {img: '../img/galeria/6.jpg'},
+        {img: '../img/galeria/7.jpg'},
+        {img: '../img/galeria/8.jpg'},
+        {img: '../img/galeria/9.jpg'},
+        {img: '../img/galeria/10.jpg'},
+        {img: '../img/galeria/11.jpg'},
+        {img: '../img/galeria/12.jpg'},
+        {img: '../img/galeria/13.jpg'},
+        {img: '../img/galeria/14.jpg'},
+        {img: '../img/galeria/15.jpg'},
+        {img: '../img/galeria/16.jpg'},
+        {img: '../img/galeria/17.jpg'},
+        {img: '../img/galeria/18.jpg'},
+        {img: '../img/galeria/19.jpg'},
+        {img: '../img/galeria/20.jpg'},
+        {img: '../img/galeria/21.jpg'},
+        {img: '../img/galeria/22.jpg'},
+        {img: '../img/galeria/23.jpg'},
+        {img: '../img/galeria/24.jpg'},
+        {img: '../img/galeria/25.jpg'}
+    ]
+
+    let contador = 0
+    const contenedor = document.querySelector('.slideshow')
+    const overlay = document.querySelector('.overlay')
+    const galeria_imagenes = docoument.querySelectorAll('.galeria img')
+    const img_slideshow = document.querySelector('.slideshow img')
+
+    contenedor.addEventListener('click', function (event) {
+        let atras = contenedor.querySelector('.atras'),
+            adelante = contenedor.querySelector('.adelante'),
+            img = contenedor.querySelector('img'),
+            tgt = event.target
+        if (tgt == atras) {
+            if (contador > 0) {
+                img.src = imagenes[contador - 1].img
+                contador--
+            } else {
+                img.src = imagenes[imagenes.length - 1].img
+                contador = imagenes.length - 1
+            }
+        } else if (tgt == adelante) {
+            if (contador < imagenes.length - 1) {
+                img.src = imagenes[contador + 1].img
+                contador++
+            } else {
+                img.src = imagenes[0].img
+                contador = 0
+            }
+        }
+    })
+
+    Array.from(galeria_imagenes).forEach(img => {
+        img.addEventListener('click', event => {
+            const imagen_seleccionada = +event.target.dataset.imgMostrar
+            img_slideshow.src = imagenes[imagen_seleccionada].img
+            contador = imagen_seleccionada
+            overlay.style.visibility = 'visible';
+            overlay.style.opacity = 1;
+        })
+    })
+
+    document.querySelector('.btn_cerrar').addEventListener('click', () => {
+        overlay.style.opacity = 0
+        overlay.style.visibility = 'hidden'
+    })
+
+
+})
+
+
